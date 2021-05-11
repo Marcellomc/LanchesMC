2bf87ad93f47f0dce0cff4e6f0c79c7f0ad1e3ff
Marcello Bianchini <mcdescartaveis@hotmail.com>
2021-05-11 00:56:19 -0300

    Criado o DbContext e string de Conexao

---
+++ LanchesMC/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LanchesMC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMC.Context
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Lanche> Lanches { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
